db62a5eab64882f20dc61732a4b318892da7b015
David Fifield <david@bamsoftware.com>
2017-02-12 16:26:52 -0800

    Keep track of and delete stray temporary files.
    Put all the handling of non-temporary file (renaming, deleting) on the
    main goroutine. The downloader functions communicate the names of
    temporary files and the results of downloads through channels.

--- ooni-sync.go
+++ ooni-sync.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
 	"path"
 	"path/filepath"
 	"strings"
@@ -34,6 +35,16 @@ const ooniAPILimit = 1000
 const numDownloadThreads = 5
 
 var outputDirectory = "."
+
+// Represents the state of a download attempt. processIndex writes these into a
+// channel and the main goroutine collects them for status updates and cleanup.
+type result struct {
+	URL           string
+	LocalFilename string
+	TmpFilename   string
+	Exists        bool
+	Err           error
+}
 
 type progressCounter struct {
 	n, total uint
@@ -87,43 +98,45 @@ func downloadToWriter(urlString string, w io.Writer) (err error) {
 	return err
 }
 
-func downloadToFile(urlString, filename string) error {
+func downloadToTmpFile(urlString string, tmpFilenameChan chan<- string) (string, error) {
 	tmpfile, err := ioutil.TempFile(outputDirectory, "ooni-sync.tmp.")
 	if err != nil {
-		return err
+		return "", err
 	}
+	// Tell the main goroutine thread to clean up this temporary file.
+	tmpFilenameChan <- tmpfile.Name()
 
 	err = downloadToWriter(urlString, tmpfile)
 	err2 := tmpfile.Close()
 	if err == nil {
 		err = err2
 	}
-	if err != nil {
-		os.Remove(tmpfile.Name())
-		return err
-	}
 
-	return os.Rename(tmpfile.Name(), filename)
+	return tmpfile.Name(), err
 }
 
-func maybeDownloadToFile(urlString, filename string) (bool, error) {
-	_, err := os.Stat(filename)
+func maybeDownload(urlString string, tmpFilenameChan chan<- string) *result {
+	r := &result{}
+	r.URL = urlString
+
+	u, err := url.Parse(r.URL)
+	if err != nil {
+		r.Err = err
+		return r
+	}
+	r.LocalFilename = filepath.Join(outputDirectory, path.Base(u.Path))
+
+	_, err = os.Stat(r.LocalFilename)
 	if err == nil {
-		return true, err
+		r.Exists = true
+		return r
 	} else if !os.IsNotExist(err) {
-		return false, err
+		r.Err = err
+		return r
 	}
-	return false, downloadToFile(urlString, filename)
-}
 
-func maybeDownload(urlString string) (bool, error) {
-	u, err := url.Parse(urlString)
-	if err != nil {
-		return false, err
-	}
-	filename := path.Base(u.Path)
-	filename = filepath.Join(outputDirectory, filename)
-	return maybeDownloadToFile(urlString, filename)
+	r.TmpFilename, r.Err = downloadToTmpFile(r.URL, tmpFilenameChan)
+	return r
 }
 
 func logOK(downloadURL string) {
@@ -147,16 +160,9 @@ func logError(downloadURL string, err error) {
 	progress.mutex.Unlock()
 }
 
-func downloadFromChan(downloadURLChan <-chan string) {
+func downloadFromChan(downloadURLChan <-chan string, resultChan chan<- *result, tmpFilenameChan chan<- string) {
 	for downloadURL := range downloadURLChan {
-		exists, err := maybeDownload(downloadURL)
-		if err != nil {
-			logError(downloadURL, err)
-		} else if exists {
-			logExists(downloadURL)
-		} else {
-			logOK(downloadURL)
-		}
+		resultChan <- maybeDownload(downloadURL, tmpFilenameChan)
 	}
 }
 
@@ -270,27 +276,74 @@ func main() {
 		os.Exit(1)
 	}
 
-	var wg sync.WaitGroup
-	downloadURLChan := make(chan string, ooniAPILimit)
-	for i := 0; i < numDownloadThreads; i++ {
-		wg.Add(1)
-		go func() {
-			downloadFromChan(downloadURLChan)
-			wg.Done()
-		}()
-	}
-
 	query, err := parseArgsToQuery(flag.Args())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
-	err = processIndex(query, downloadURLChan)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		os.Exit(1)
+
+	tmpFilenameChan := make(chan string)
+	resultChan := make(chan *result)
+
+	downloadURLChan := make(chan string, ooniAPILimit)
+	go func() {
+		err = processIndex(query, downloadURLChan)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			os.Exit(1)
+		}
+		close(downloadURLChan)
+	}()
+	var wg sync.WaitGroup
+	wg.Add(numDownloadThreads)
+	for i := 0; i < numDownloadThreads; i++ {
+		go func() {
+			downloadFromChan(downloadURLChan, resultChan, tmpFilenameChan)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	// Keep track of temporary files we need to delete at the end.
+	tmpFilenames := make(map[string]struct{})
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+
+loop:
+	for {
+		select {
+		case tmpFilename := <-tmpFilenameChan:
+			tmpFilenames[tmpFilename] = struct{}{}
+		case r, ok := <-resultChan:
+			if !ok {
+				break loop
+			}
+			if r.Err != nil {
+				logError(r.URL, r.Err)
+			} else if r.Exists {
+				logExists(r.URL)
+			} else {
+				err := os.Rename(r.TmpFilename, r.LocalFilename)
+				if err != nil {
+					logError(r.URL, err)
+				} else {
+					delete(tmpFilenames, r.TmpFilename)
+					logOK(r.URL)
+				}
+			}
+		case <-sigChan:
+			break loop
+		}
 	}
 
-	close(downloadURLChan)
-	wg.Wait()
+	for tmpFilename := range tmpFilenames {
+		err := os.Remove(tmpFilename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cannot delete temporary: %s", err)
+		}
+	}
 }
